23545703aa72ed7bb0df54692e7f61dc093deead
aungthetko <aungthetko.cue.mm@gmail.com>
2022-07-22 00:10:30 +0630

    src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+
+  const expenses = [
+    {
+      id: "e1",
+      name: "Office Desk (Wooden)",
+      price: 150.32,
+      date: new Date(2021, 2, 28)
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ExpenseItem 
+        name={expenses[0].name}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
     </div>
   );
 }
