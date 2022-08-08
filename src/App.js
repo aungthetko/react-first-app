88822426451fac86563e7680f047313f7ca9dfba
aungthetko <aungthetko.cue.mm@gmail.com>
2022-08-09 00:13:32 +0630

    expense-date-component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
       name: "Office Desk (Wooden)",
       price: 150.32,
       date: new Date(2021, 2, 28)
+    },
+    {
+      id: "e2",
+      name: "Smart TV",
+      price: 120.51,
+      date: new Date(2022, 1, 3)
     }
   ];
 
@@ -18,6 +24,11 @@ function App() {
         name={expenses[0].name}
         price={expenses[0].price}
         date={expenses[0].date}
+      />
+      <ExpenseItem 
+        name={expenses[1].name}
+        price={expenses[1].price}
+        date={expenses[1].date}
       />
     </div>
   );
